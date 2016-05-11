b4166c45c2587eb97eb910adf0d913943c2c05a0
Jirayu Kanda <me@acodify.com>
2016-05-11 17:01:44 +0700

    make show message error

--- src/database/DB_driver.php
+++ src/database/DB_driver.php
@@ -1696,7 +1696,15 @@ abstract class CI_DB_driver {
 
 //		$error =& load_class('Exceptions', 'core');
 //		echo $error->show_error($heading, $message, 'error_db');
-		echo $message;
+
+        if (!empty($message)) {
+            foreach ($message as $key => $value) {
+                echo '<pre>';
+                print_r( $value );
+                echo '</pre>';
+            }
+        }
+        echo "Database Error";
 		exit(8); // EXIT_DATABASE
 	}
 
